d50405addaa5e57a901c9e1e67568229e3360003
Abu Suraj <abusurajbd@gmail.com>
2024-01-13 19:31:29 +0600

    change anchor tag with span

--- src/pages/sharedPage/navbar/Header.js
+++ src/pages/sharedPage/navbar/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
         <div>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                    <a className="navbar-brand">Employee-Easee</a>
+                    <span className="navbar-brand">Employee-Easee</span>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
